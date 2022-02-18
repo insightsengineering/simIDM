c24562b26c437e402cae1571ab5183a160b6e9e1
niessl <64351230+niessl@users.noreply.github.com>
2022-02-18 14:46:51 +0100

    12 add get sum pcw (#13)
    * first commit
    
    * complete issue #8
    
    * small change to trigger precommit
    
    * interim
    
    * interim
    
    * interim
    
    * # clean-up
    
    * resolve git warnings
    
    * debugged (seq_along instead of seq_len)
    
    * update
    
    * Apply suggestions from code review
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * applying review comments
    
    Co-authored-by: Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- R/piecewiseHazards.R
+++ R/piecewiseHazards.R
@@ -7,7 +7,7 @@
 #' @param pw (`numeric`)\cr time intervals for the piecewise constant hazard.
 #' @param x (`numeric`)\cr time-points.
 #'
-#' @return hazard values at input time-points.
+#' @return Hazard values at input time-points.
 #' @export
 #'
 #' @examples
@@ -15,7 +15,7 @@
 getPCWHazard <- function(haz, pw, x) { # nolint
   haz_val <- sapply(x, function(jj) {
     y <- NULL
-    # find interval and corresponding hazard value for time x[jj]
+    # Find interval and corresponding hazard value for time x[jj].
     for (ii in seq_along(haz)) {
       if (jj >= pw[ii]) {
         y <- haz[ii]
@@ -25,4 +25,34 @@ getPCWHazard <- function(haz, pw, x) { # nolint
     return(haz_val)
   })
   return(haz_val)
+}
+
+
+
+
+
+#' Sum of two piecewise constant hazards
+#'
+#' This returns the sum of two piecewise constant hazards per interval.
+#'
+#' @param haz1 (`numeric`)\cr first summand (piecewise constant hazard).
+#' @param haz2 (`numeric`)\cr second summand (piecewise constant hazard).
+#' @param pw1 (`numeric`)\cr time intervals of first summand.
+#' @param pw2  (`numeric`)\cr time intervals of second summand.
+#'
+#' @return List with elements `hazards` and  `intervals` for the sum of two piecewise constant hazards.
+#' @export
+#'
+#' @examples
+#' getSumPCW(c(1.2, 0.3, 0.6), c(1.2, 0.7, 1), c(0, 8, 9), c(0, 1, 4))
+getSumPCW <- function(haz1, haz2, pw1, pw2) {
+  # Get all cutpoints for the intervals.
+  cuts_sum <- unique(sort(c(pw1, pw2)))
+  haz_sum <- NULL
+  # Get sum of hazards for all intervals.
+  for (i in seq_along(cuts_sum)) {
+    haz_sum[i] <- getPCWHazard(haz1, pw1, cuts_sum[i]) +
+      getPCWHazard(haz2, pw2, cuts_sum[i])
+  }
+  return(list(hazards = haz_sum, intervals = cuts_sum))
 }
--- tests/testthat/test-piecewiseHazards.R
+++ tests/testthat/test-piecewiseHazards.R
@@ -4,3 +4,10 @@ test_that("getPCWHazard works as expected", {
   actual <- getPCWHazard(c(0.8, 1.1, 1), c(0, 5, 8), c(1, 5, 7, 9))
   expect_identical(actual, c(0.8, 1.1, 1.1, 1))
 })
+
+
+# getSumPCW ----
+test_that("getSumPCW works as expected", {
+  actual <- getSumPCW(c(0.8, 1.1, 1), c(0.8, 1.1, 1, 0.4), c(0, 5, 8), c(0, 3, 7, 9))
+  expect_equal(actual, list(hazards = c(1.6, 1.9, 2.2, 2.1, 2, 1.4), intervals = c(0, 3, 5, 7, 8, 9)))
+})
