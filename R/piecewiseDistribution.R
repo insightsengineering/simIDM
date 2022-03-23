81f581f7d7fd7e97737c56cc9749190a582ff829
niessl <64351230+niessl@users.noreply.github.com>
2022-03-23 09:49:33 +0100

    issue 15 (#16)
    * issue 15
    
    * Apply suggestions from code review
    
    accept suggestions
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * review comments included
    
    * Apply suggestions from code review
    
    accept suggestions of review
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * working on review comments
    
    * review comments: exited loop early
    
    * use findInterval instead of for loop
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

---
+++ R/piecewiseDistribution.R
@@ -0,0 +1,85 @@
+
+#' Piecewise exponentially distributed event times
+#'
+#' This returns event times with a distribution resulting from piece-wise constant hazards
+#' using the inversion method.
+#'
+#' @param U (`numeric`)\cr uniformly distributed random variables.
+#' @param haz (`numeric`)\cr piecewise constant hazard.
+#' @param pw (`numeric`)\cr time intervals for the piecewise constant hazard.
+#' @param t_0 (`numeric`)\cr the starting times.
+#'
+#' @return This returns a vector with event times.
+#' @export
+#'
+#' @examples
+#' getPCWDistr(U = runif(3), haz = c(1.1, 0.5, 0.4), pw = c(0, 7, 10), t_0 = c(0, 1, 4.2))
+getPCWDistr <- function(U, haz, pw, t_0) {
+  assert_numeric(haz, lower = 0, any.missing = FALSE, all.missing = FALSE)
+  assert_intervals(pw, length(haz))
+  assert_numeric(t_0, lower = 0, len = length(U), any.missing = FALSE, all.missing = FALSE)
+  assert_numeric(U, lower = 0, upper = 1, any.missing = FALSE, all.missing = FALSE)
+
+  N <- length(U)
+  t1 <- rep(NA, N) # Initialize event times.
+  n2 <- length(haz)
+  for (kk in seq_len(N)) {
+    # Shift if t_0 !=0.
+    cuts_temp <- c(t_0[kk], pw[pw > t_0[kk]])
+    cuts_temp <- cuts_temp - t_0[kk]
+    # Number of cut-points after shift.
+    n <- length(cuts_temp)
+    # Number of hazards to remove for shift (t_0 > times).
+    remov <- n2 - n
+    haz_temp <- haz[(remov + 1):n2]
+    LogU <- log(1 - U[kk])
+
+    t1[kk] <- if (n != 1) {
+      PCWInversionMethod(haz = haz_temp, pw = cuts_temp, LogU = LogU)
+    } else if (n == 1) { # I.e. exponential distribution.
+      -LogU / haz_temp
+    }
+  }
+  return(t1)
+}
+
+
+
+#' A single piecewise exponentially distributed event time.
+#'
+#' This returns an event time with a distribution resulting from piece-wise constant hazards
+#' using the inversion method.
+#'
+#' @param pw (`numeric`)\cr time intervals for the piecewise constant hazard.
+#' @param haz (`numeric`)\cr piecewise constant hazard.
+#' @param LogU (`numeric`)\cr transformed uniformly distributed random variables (log(1-U)).
+#'
+#' @return This returns one single event time.
+#' @export
+#'
+#' @examples
+#' PCWInversionMethod(haz = c(1.1, 0.5, 0.4), pw = c(0, 7, 10), LogU = log(1 - runif(1)))
+PCWInversionMethod <- function(haz, pw, LogU) {
+  assert_numeric(haz, lower = 0, any.missing = FALSE, all.missing = FALSE)
+  assert_intervals(pw, length(haz))
+  assert_number(LogU)
+
+  n <- length(pw)
+  t1 <- 0
+  # Determine sum of alpha*time-interval for all i.
+  dt <- pw[2:n] - pw[1:(n - 1)]
+
+  # Helping matrix.
+  tempMatrix <- matrix(0, nrow = n, ncol = n - 1)
+  tempMatrix[lower.tri(tempMatrix)] <- 1
+
+  sumA <- -as.vector(tempMatrix %*% (haz[1:(n - 1)] * dt))
+  # Find the appropriate time interval.
+  rev_index <- findInterval(LogU, rev(sumA), rightmost.closed = FALSE, left.open = TRUE, all.inside = FALSE)
+  # Use reverse index.
+  index <- n - rev_index
+  # Calculate event time.
+  t1 <- pw[index] + (sumA[index] - LogU) / haz[index]
+
+  return(t1)
+}
---
+++ tests/testthat/test-piecewiseDistrb.R
@@ -0,0 +1,36 @@
+# getPCWdistr ----
+test_that("getPCWDistr works as expected for constant hazards after time shift", {
+  U <- 0.65
+  actual <- getPCWDistr(U, c(1.1, 0.8), c(0, 5), 6)
+  expected <- -log(1 - U) / 0.8
+  expect_equal(actual, expected)
+})
+
+test_that("getPCWDistr works as expected for pwc hazards without time shift", {
+  U <- 0.91
+  actual <- getPCWDistr(U = U, haz = c(1.1, 0.5, 0.4), pw = c(0, 0.6, 5), t_0 = 0)
+  # 1-survival function of the actual time-points.
+  expected <- 1 - exp(-(1.1 * 0.6) - (actual - 0.6) * 0.5)
+  expect_equal(U, expected)
+})
+
+
+test_that("getPCWDistr works as expected for piecewise constant hazards with time shift", {
+  U <- c(0.45, 0.8)
+  actual <- getPCWDistr(U = U, haz = c(1.1, 0.5, 0.4), pw = c(0, 0.6, 5), t_0 = c(4.2, 4.2))
+  # 1-survival function of the actual time-points.
+  expected <- 1 - exp(-(0.8 * 0.5) - (actual + 4.2 - 5) * 0.4)
+  expect_equal(U, expected)
+})
+
+
+# PCWInversionMethod ----
+
+test_that("PCWInversionMethod works as expected", {
+  U <- 0.4
+  logU <- log(1 - U)
+  actual <- PCWInversionMethod(haz = c(1.1, 0.5, 0.4), pw = c(0, 0.6, 5), LogU = logU)
+  # 1-survival function of the actual time-point.
+  expected <- 1 - exp(-(actual * 1.1))
+  expect_equal(U, expected)
+})
