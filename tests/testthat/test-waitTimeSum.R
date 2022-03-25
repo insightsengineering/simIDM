1d16e3a59bad9bb3d056fde5c1a0f1d473f1f263
Alexandra Niessl <alexandra.niessl@uni-ulm.de>
2022-03-25 10:34:49 +0100

    Applying review suggestions

--- tests/testthat/test-waitTimeSum.R
+++ tests/testthat/test-waitTimeSum.R
@@ -8,12 +8,11 @@ test_that("getWaitTimeSum  works as expected for sum of constant hazards", {
 
 
 test_that("getWaitTimeSum  works as expected for sum of hazards (entry = 0)", {
-  U <- 0.23
-  actual <- getWaitTimeSum(U, haz1 = 0.8, haz2 = 1.2, p1 = 1.5, p2 = 0.7, entry = 0)
-  expected <- (0.8 * actual)^1.5 + (1.2 * actual)^0.7
+  U <- c(0.23, 0.86)
+  actual <- getWaitTimeSum(U, haz1 = 0.8, haz2 = 1.2, p1 = 1.5, p2 = 0.7, entry = c(0, 0))
+  expected <- c((0.8 * actual[1])^1.5 + (1.2 * actual[1])^0.7, (0.8 * actual[2])^1.5 + (1.2 * actual[2])^0.7)
   expect_equal(-log(1 - U), expected)
 })
-
 
 test_that("getWaitTimeSum  works as expected for sum of hazards (entry != 0)", {
   U <- 0.4
