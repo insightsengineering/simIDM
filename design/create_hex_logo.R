cf23561e475add2415dd73bbd9a9e020065192e3
Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
2023-12-11 15:05:20 +0100

    adding better png logo versions (#103)

--- design/create_hex_logo.R
+++ design/create_hex_logo.R
@@ -1,6 +1,7 @@
 pkg_color <- "red"
 fill_color <- "lightgrey"
-out_path <- file.path(getwd(), "man/figures/logo-large.png")
+out_path <- file.path(getwd(), "man/figures/logo-large")
+out_file <- paste(out_path, "png", sep = ".")
 
 idm_plot <- function() {
   prodlim::plotIllnessDeathModel(
@@ -16,9 +17,9 @@ idm_plot <- function() {
 }
 idm_plot()
 
-idm_fig <- file.path(tmp, "idm_plot.png")
 tmp <- tempdir()
-png(filename = idm_fig, res = 200, width = 500, height = 500, bg = "transparent")
+idm_fig <- file.path(tmp, "idm_plot.png")
+png(filename = idm_fig, res = 100, width = 500, height = 500, bg = "transparent")
 idm_plot()
 dev.off()
 
@@ -44,12 +45,19 @@ make_hexplot <- function() {
     url = "github.com/insightsengineering/simIDM",
     u_size = 24,
     u_color = pkg_color,
-    filename = out_path,
+    filename = out_file,
     p_family = "mono",
+    # We need this white around sticker, otherwise the top and bottom of the
+    # hexagon are not nice (too flat).
     white_around_sticker = TRUE,
     dpi = 2000
   )
+  # But therefore we now need to postprocess the image by transforming white
+  # to transparent background.
+  out_img <- magick::image_read(out_file)
+  out_img <- magick::image_transparent(out_img, color = "white")
+  magick::image_write(out_img, path = out_file)
 }
 
 make_hexplot()
-usethis::use_logo(out_path)
+usethis::use_logo(out_file)
