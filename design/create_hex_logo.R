569fa3b192c49c45be06e2d6221cb9a4491ea87d
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2023-12-11 14:34:24 +0100

    also produce svg

--- design/create_hex_logo.R
+++ design/create_hex_logo.R
@@ -1,6 +1,6 @@
 pkg_color <- "red"
 fill_color <- "lightgrey"
-out_path <- file.path(getwd(), "man/figures/logo-large.png")
+out_path <- file.path(getwd(), "man/figures/logo-large")
 
 idm_plot <- function() {
   prodlim::plotIllnessDeathModel(
@@ -16,9 +16,9 @@ idm_plot <- function() {
 }
 idm_plot()
 
-idm_fig <- file.path(tmp, "idm_plot.png")
 tmp <- tempdir()
-png(filename = idm_fig, res = 200, width = 500, height = 500, bg = "transparent")
+idm_fig <- file.path(tmp, "idm_plot.png")
+png(filename = idm_fig, res = 100, width = 500, height = 500, bg = "transparent")
 idm_plot()
 dev.off()
 
@@ -28,28 +28,30 @@ make_hexplot <- function() {
   require(hexSticker)
   require(showtext)
 
-  hexSticker::sticker(
-    subplot = idm_fig,
-    package = "simIDM",
-    p_size = 140,
-    p_color = pkg_color,
-    p_y = 1.5,
-    s_y = 0.8,
-    s_x = 0.9,
-    s_width = 0.48,
-    s_height = 0.48,
-    h_fill = fill_color,
-    h_color = pkg_color,
-    h_size = 2,
-    url = "github.com/insightsengineering/simIDM",
-    u_size = 24,
-    u_color = pkg_color,
-    filename = out_path,
-    p_family = "mono",
-    white_around_sticker = TRUE,
-    dpi = 2000
-  )
+  for (out_file in paste(out_path, c("svg", "png"), sep = ".")) {
+    hexSticker::sticker(
+      subplot = idm_fig,
+      package = "simIDM",
+      p_size = 140,
+      p_color = pkg_color,
+      p_y = 1.5,
+      s_y = 0.8,
+      s_x = 0.9,
+      s_width = 0.48,
+      s_height = 0.48,
+      h_fill = fill_color,
+      h_color = pkg_color,
+      h_size = 2,
+      url = "github.com/insightsengineering/simIDM",
+      u_size = 24,
+      u_color = pkg_color,
+      filename = out_file,
+      p_family = "mono",
+      white_around_sticker = TRUE,
+      dpi = 2000
+    )
+  }
 }
 
 make_hexplot()
-usethis::use_logo(out_path)
+usethis::use_logo(paste0(out_path, ".png"))
